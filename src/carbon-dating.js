1aaad45d54c53d29421c581e9910ab6f6765e54d
Xuanita <alabama10@inbox.lv>
2023-12-15 13:53:10 +0200

    feat: carbon dating done

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,18 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  const sampleActivityNumb = parseFloat(sampleActivity);
+
+  if (typeof sampleActivity !== 'string' ||
+    isNaN(sampleActivityNumb) ||
+    sampleActivityNumb <= 0 ||
+    sampleActivityNumb > MODERN_ACTIVITY) {
+    return false;
+  }
+
+  const archAge = Math.log(MODERN_ACTIVITY / sampleActivityNumb) / (0.693 / HALF_LIFE_PERIOD);
+  return Math.ceil(archAge);
 }
 
 module.exports = {
